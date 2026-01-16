d64bbc7cccc6d01a7476e674350dab560d366eb0
root <root@rhost.local.vrqq.org>
2026-01-16 13:21:10 +0800

    add patch file

--- linux-5.14.0-611.20.1.el9_7/clean.sh
+++ linux-5.14.0-611.20.1.el9_7/clean.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 rm -fr .megaraid* megaraid_sas_pdoff.mod.* megaraid_sas_pdoff.ko *.mod *.o \
-    .tmp_versions .modules* .Module* modules.order Module.symvers output.log
+    .tmp_versions .modules* .Module* modules.order Module.symvers output.log \
+    *.ko.xz
